ba8130734f4f30e51f7f870dedf2ebd311a0da77
RonyKader <rony.ksr06@gmial.com>
2015-10-11 11:46:00 +0100

    2nd up

--- core/database/db_connection.php
+++ core/database/db_connection.php
@@ -2,17 +2,17 @@
 // Create a Database Connection 
 	$dbhost 	= "localhost";
 	$dbuser		= "root";
-	$dbpassword	= "root";
-	$dbname		= "watch";
+	$dbpassword	= "";
+	$dbname		= "tr_shop";
 
 	$connection = mysqli_connect( $dbhost, $dbuser, $dbpassword, $dbname );
 
 	if ( mysqli_connect_errno() ) {
 		die( "Database could't connect and error is : " . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")" );
 	}
-	else{
-		echo "Sucess";
-	}
+	// else{
+	// 	echo "Sucess";
+	// }
 //Query Perform
 
  ?>
