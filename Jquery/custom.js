b8e733fd9b5cc62abbe0a7a184e4f320c3015fa2
RonyKader <rony.ksr06@gmial.com>
2015-10-11 15:21:44 +0100

    show menu

--- Jquery/custom.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$('li').filter(':nth-child(n+3)').addClass('nhied');
-});
--- admin-template/admin_nav.php
+++ admin-template/admin_nav.php
@@ -1,72 +1,50 @@
 <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 	<div class="admin-nav">
 	<h2><a href="admin.php">Admin Menu</a></h2>
-		<nav>
-			<ul class="list-unstyled">
-				<?php 
-				if ( isset( $_GET[ 'subject' ] )) {
-					
-					$get_subject_id = $_GET[ 'subject' ];
-					$get_page_id = NULL;
-				}
-				elseif ( isset( $_GET[ 'page' ] )) {
-					$get_page_id = $_GET[ 'page' ];
-					$get_subject_id = NULL;
-				}
-				else{
-					$get_subject_id = NULL;
-					$get_page_id = NULL;
+		<nav class="admin-nav">
+			<div class="bs-example">
+            <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                <?php 
+                	$sql_view = "SELECT * FROM menus";
+                	$sql_query = mysqli_query( $connection, $sql_view );               	
+                	 
+                	 while ( $row = $row = mysqli_fetch_assoc( $sql_query ) ) {
+                	 	?>
+	                    <a style="display:inline-block; width:100%;" class="chef-panel panel-heading panel-title" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $row[ 'id' ];?>">
+	                    	<i class="fa fa-hand-o-right"></i> <?php echo $row[ 'menu_name' ];?>
+	                    </a>    
+	                    <div id="<?php echo $row[ 'id' ];?>" class="panel-collapse collapse">
+	                    <?php 
+	                    	$sql_sub = "SELECT * FROM category WHERE page_id = {$row['id']}";
+	                    	$qu = mysqli_query( $connection, $sql_sub );
 
-				}
-					$view_result = find_all_mainnav();
-					while ( $row = mysqli_fetch_assoc( $view_result ) ) {
-				?>
-						<li>
-						<a href="admin.php?subject=<?php echo urlencode( $row[ 'id' ] ); ?>"><?php echo $row[ 'menu_name' ]; ?></a>
-							
+	                    	while ( $sub = mysqli_fetch_assoc( $qu ) ) {
+	                    		?>
 
-							<?php 
-							$result_page = find_pagefor_mainnav( $row[ 'id' ] );
-							?>	
-							<ul class="list-unstyled">
-							<?php
-							while ( $page_row = mysqli_fetch_assoc( $result_page )) {
-							?>
-								<li class="<?php if( $get_page_id == $page_row[ 'id' ] ) { echo "page-selected";} ?>">
-									<span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>
-									<a href="admin.php?page=<?php echo urlencode( $page_row[ 'id' ] ); ?>"><?php echo $page_row[ 'menu_name' ]; ?></a>
-								</li>
+	                        <div class="panel-body">
+	                            <ul class="list-unstyled">
+	                            	<li><a href="#"><?php echo $sub['cat_name']; ?></a></li>
+	                            	
+	                            </ul>
+	                        </div>
 
-								<?php
-								
-							}
-							mysqli_free_result( $result_page );
+	                    		<?php
+	                    	}
+	                     ?>                 
+	                    </div>
 
-							 ?>
-								
-							</ul>
-						</li>
-						<?php
+                	 	<?
+                	 }
 
-					}
-						mysqli_free_result( $view_result );
-					
-				 ?>
-						<li><a href="create-subject.php">Create New Subject</a></li>
-				<!-- <li><a href="#">MANAGE CONTENT</a>
-					<ul class="list-unstyled">
-						<li><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span> <a href="#">Navigation</a></li>
-						<li><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span> <a href="#">Subject CRUD</a></li>
-						<li><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span> <a href="#">Page CRUD</a></li>
-					</ul>
-				</li>
-				<li><a href="#">MANAGE ADMIN</a>
-					<ul class="list-unstyled">
-						<li><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span> <a href="#">Admin CRUD</a></li>
-					</ul>
-				</li>
-				<li><a href="logout.php">LOGOUT</a></li> -->
-			</ul>
+                 ?>                              
+           
+                </div>
+             <!-- End of Question section one -->
+                
+            </div>
+        </div>
 		</nav>
+		<h2><a href="#">Create a Menu</a></h2>
 	</div>
 </div>
