60c9f232161a127f512cbb07216684380c649613
RonyKader <rony.ksr06@gmial.com>
2015-10-11 16:30:27 +0100

    function

--- core/functions/general.php
+++ core/functions/general.php
@@ -16,19 +16,4 @@ function show_category( $menu_id ){
 }
 
 
-// FUNCTION FOR MAIN NAVE
-function showNavTitle() {
-	global $connection;
-	$sql_view = "SELECT * FROM menus";
-    $sql_query = mysqli_query( $connection, $sql_view );
-    return $sql_query;
-}
-// FUNCTION FOR CATEGORY
-function show_category( $menu_id ){
-	global $connection;
-	$sql_view = "SELECT * FROM `category` WHERE page_id = {$menu_id}";
-    $sql_query = mysqli_query( $connection, $sql_view );
-    return $sql_query;
-}
-
 
